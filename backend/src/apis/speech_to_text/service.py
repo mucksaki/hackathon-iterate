414b6c4c75f71e2464698f74fe0f617b35291b18
ilyass <ilyass.mejdoub@pm.me>
2025-12-14 13:14:33 +0000

    explicit str name

--- backend/src/apis/speech_to_text/service.py
+++ backend/src/apis/speech_to_text/service.py
@@ -6,9 +6,9 @@ class SttService:
     def stt(self, input: SpeechToTextInput) -> List[dict[str, str]]:
         print('upload start ...')
         stt_input = input
-        object_key = upload_wav_to_pyannote(file_path=stt_input.file_path, object_key=stt_input.object_key)
+        object_key_str = upload_wav_to_pyannote(file_path=stt_input.file_path, object_key=stt_input.object_key)
         print('upload finished')
         print('output start...')
-        output = get_text(object_key=object_key)
+        output = get_text(object_key=object_key_str)
         print('output finished')
         return output
